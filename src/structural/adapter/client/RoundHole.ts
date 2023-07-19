ff78cb6e51b9dc9080f8b253c3034fe6df9ed765
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-07-19 17:34:36 +0000

    refactor: round hole no longer implements round shape

--- src/structural/adapter/client/RoundHole.ts
+++ src/structural/adapter/client/RoundHole.ts
@@ -1,13 +1,10 @@
 import RoundShape from "../client_interface/RoundShape";
 
-export default class RoundHole implements RoundShape {
-    constructor(private radius: number) {
+export default class RoundHole {
+    constructor(readonly radius: number) {
         if (this.radius <= 0) throw new Error("Radius must be greater than 0");
     }
-    getRadius(): number {
-        return this.radius;
-    }
-    fits(peg: RoundShape): boolean {
-        return this.getRadius() >= peg.getRadius();
+    checkFitsHole(peg: RoundShape): boolean {
+        return this.radius >= peg.getRadius();
     }
 }
