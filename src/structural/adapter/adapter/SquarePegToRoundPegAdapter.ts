bff4ca63294895a87a608907f07d45e8d3b661e4
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-07-19 17:32:40 +0000

    refactor: change square peg adapter name to square peg to round peg adapter

--- src/structural/adapter/adapter/SquarePegToRoundPegAdapter.ts
+++ src/structural/adapter/adapter/SquarePegToRoundPegAdapter.ts
@@ -1,7 +1,7 @@
 import RoundShape from "../client_interface/RoundShape";
 import SquarePeg from "../service/SquarePeg";
 
-export default class SquarePegAdapter implements RoundShape {
+export default class SquarePegToRoundPegAdapter implements RoundShape {
     constructor(private squarePeg: SquarePeg) {}
     getRadius(): number {
         return this.squarePeg.getWidth() * (Math.sqrt(2) / 2);
