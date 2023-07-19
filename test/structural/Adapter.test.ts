32bffe071b4b7222a0b8ff563c983ea86ee5b400
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-07-19 17:36:24 +0000

    fix: update square peg to round peg adapter import

--- test/structural/Adapter.test.ts
+++ test/structural/Adapter.test.ts
@@ -1,24 +1,24 @@
 import RoundHole from "../../src/structural/adapter/client/RoundHole";
 import RoundPeg from "../../src/structural/adapter/service/RoundPeg";
 import SquarePeg from "../../src/structural/adapter/service/SquarePeg";
-import SquarePegAdapter from "../../src/structural/adapter/adapter/SquarePegAdapter";
+import SquarePegToRoundPegAdapter from "../../src/structural/adapter/adapter/SquarePegToRoundPegAdapter";
 
 test("Should return a round peg that fits in round hole", function () {
     const roundHole = new RoundHole(5);
     const roundPeg = new RoundPeg(5);
-    expect(roundHole.fits(roundPeg)).toEqual(true);
+    expect(roundHole.checkFitsHole(roundPeg)).toEqual(true);
 });
 
 test("Should return a square peg that fits in round hole", function () {
     const roundHole = new RoundHole(5);
     const squarePeg = new SquarePeg(5); // radius = 3.53553
-    const squarePegAdapted = new SquarePegAdapter(squarePeg);
-    expect(roundHole.fits(squarePegAdapted)).toEqual(true);
+    const squarePegAdapted = new SquarePegToRoundPegAdapter(squarePeg);
+    expect(roundHole.checkFitsHole(squarePegAdapted)).toEqual(true);
 });
 
 test("Should return a square peg that does not fit in round hole", function () {
     const roundHole = new RoundHole(5);
     const squarePeg = new SquarePeg(10); // radius = 7.07107
-    const squarePegAdapted = new SquarePegAdapter(squarePeg);
-    expect(roundHole.fits(squarePegAdapted)).toEqual(false);
+    const squarePegAdapted = new SquarePegToRoundPegAdapter(squarePeg);
+    expect(roundHole.checkFitsHole(squarePegAdapted)).toEqual(false);
 });
