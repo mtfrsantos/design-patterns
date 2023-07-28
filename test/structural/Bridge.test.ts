62d19a209aa54ae3d5d391322684b6f228f0f8a2
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-07-28 12:29:25 +0000

    refactor[test]: break bridge test into multiples

--- test/structural/Bridge.test.ts
+++ test/structural/Bridge.test.ts
@@ -3,14 +3,34 @@ import Radio from "../../src/structural/bridge/concrete_implementation/Radio";
 import RemoteControl from "../../src/structural/bridge/abstraction/RemoteControl";
 import TV from "../../src/structural/bridge/concrete_implementation/TV";
 
-test("Should validate the TV and Radio", function () {
+test("Should validate the TV remote control", function () {
     const tv = new TV();
-    const radio = new Radio();
-    const tvRemoteControl = new RemoteControl(tv);
-    const radioRemoteControl = new AdvancedRemoteControl(radio);
-    tvRemoteControl.togglePower();
-    radioRemoteControl.togglePower();
-    radioRemoteControl.mute();
+    const remoteControl = new RemoteControl(tv);
+    remoteControl.togglePower();
     expect(tv.isEnabled()).toEqual(true);
+});
+
+test("Should validate the TV advanced remote control", function () {
+    const tv = new TV();
+    const advancedRemoteControl = new AdvancedRemoteControl(tv);
+    advancedRemoteControl.togglePower();
+    advancedRemoteControl.mute();
+    expect(tv.isEnabled()).toEqual(true);
+    expect(tv.getVolume()).toEqual(0);
+});
+
+test("Should validate the Radio remote control", function () {
+    const radio = new Radio();
+    const remoteControl = new RemoteControl(radio);
+    remoteControl.togglePower();
+    expect(radio.isEnabled()).toEqual(true);
+});
+
+test("Should validate the Radio advanced remote control", function () {
+    const radio = new Radio();
+    const advancedRemoteControl = new AdvancedRemoteControl(radio);
+    advancedRemoteControl.togglePower();
+    advancedRemoteControl.mute();
+    expect(radio.isEnabled()).toEqual(true);
     expect(radio.getVolume()).toEqual(0);
 });
