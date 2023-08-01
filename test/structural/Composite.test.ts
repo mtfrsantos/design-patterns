a10b61c20640a79b8ecdc145648bcb44bc4608e2
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-01 18:45:18 -0300

    refactor[test]: add one box inside another sequentially

--- test/structural/Composite.test.ts
+++ test/structural/Composite.test.ts
@@ -1,21 +1,21 @@
-import ConcreteProduct from "../../src/structural/composite/ConcreteProduct";
-import ProductBox from "../../src/structural/composite/ProductBox";
+import ConcreteProduct from "../../src/structural/composite/component/ConcreteProduct";
+import ProductBox from "../../src/structural/composite/container/ProductBox";
 
 test("Should return the price of all products in the box", function () {
     const hammer = new ConcreteProduct("hammer", 40);
     const phone = new ConcreteProduct("phone", 2000);
     const headphone = new ConcreteProduct("Headphone", 300);
     const phoneCharger = new ConcreteProduct("phoneCharger", 100);
-    const box1 = new ProductBox();
-    box1.add(hammer);
-    const box2 = new ProductBox();
-    box2.add(phone);
-    box2.add(headphone);
-    const box3 = new ProductBox();
-    box3.add(phoneCharger);
+    const smallBox = new ProductBox();
+    smallBox.add(hammer);
+    const mediumBox = new ProductBox();
+    mediumBox.add(phone);
+    mediumBox.add(headphone);
+    mediumBox.add(smallBox);
+    const bigBox = new ProductBox();
+    bigBox.add(phoneCharger);
+    bigBox.add(mediumBox);
     const largeBox = new ProductBox();
-    largeBox.add(box1);
-    largeBox.add(box2);
-    largeBox.add(box3);
+    largeBox.add(bigBox);
     expect(largeBox.getPrice()).toEqual(2440);
 });
