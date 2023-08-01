abec3439c0906399d159f861dbf8320945bf104c
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-01 18:00:32 -0300

    refactor: move ConcreteProduct to component directory

--- src/structural/composite/component/ConcreteProduct.ts
+++ src/structural/composite/component/ConcreteProduct.ts
@@ -1,4 +1,4 @@
-import Product from "./Product";
+import Product from "../leaf/Product";
 
 export default class ConcreteProduct implements Product {
     constructor(readonly name: string, private price: number) {}
