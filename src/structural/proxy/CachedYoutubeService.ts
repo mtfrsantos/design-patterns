c8f3b7d83ceea8bfdc0b7d4ec0225a2b40e6d817
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-07 17:10:30 -0300

    refactor: add spy and three videos

--- src/structural/proxy/CachedYoutubeService.ts
+++ src/structural/proxy/CachedYoutubeService.ts
@@ -1,19 +1,25 @@
+import Spy from "./Spy";
 import ThirdPartyYoutubeLib from "./ThirdPartyYoutubeLib";
 
 export default class CachedYoutubeService implements ThirdPartyYoutubeLib {
     private videoCache: Map<string, { title: string; length: string }>;
-    constructor(private youtubeService: ThirdPartyYoutubeLib) {
+    constructor(
+        private youtubeService: ThirdPartyYoutubeLib,
+        private spy: Spy
+    ) {
         this.videoCache = new Map();
     }
-    getVideoInfo(videoId: string): { title: string; length: string } {
+    getVideoInfo(
+        videoId: string
+    ): { title: string; length: string } | undefined {
+        this.spy.execute();
         let cachedVideoInfo = this.videoCache.get(videoId);
         if (!cachedVideoInfo) {
             cachedVideoInfo = this.youtubeService.getVideoInfo(videoId);
+        }
+        if (cachedVideoInfo) {
             this.videoCache.set(videoId, cachedVideoInfo);
         }
         return cachedVideoInfo;
-    }
-    downloadVideo(videoId: string): string {
-        return `Downloading https://www.youtube.com/watch?v=${videoId}`;
     }
 }
--- src/structural/proxy/ThirdPartyYoutubeService.ts
+++ src/structural/proxy/ThirdPartyYoutubeService.ts
@@ -1,12 +1,27 @@
+import Spy from "./Spy";
 import ThirdPartyYoutubeLib from "./ThirdPartyYoutubeLib";
 export default class ThirdPartyYoutubeService implements ThirdPartyYoutubeLib {
-    getVideoInfo(videoId: string): { title: string; length: string } {
-        return {
+    private videos: Map<string, { title: string; length: string }>;
+
+    constructor(private spy: Spy) {
+        this.videos = new Map();
+        this.videos.set("9bZkp7q19f0", {
+            title: "PSY - GANGNAM STYLE(강남스타일) M/V",
+            length: "4:12",
+        });
+        this.videos.set("dQw4w9WgXcQ", {
             title: "Rick Astley - Never Gonna Give You Up (Official Music Video)",
             length: "3:32",
-        };
+        });
+        this.videos.set("foE1mO2yM04", {
+            title: "Mike Posner - I Took A Pill In Ibiza (Seeb Remix) (Explicit)",
+            length: "3:56",
+        });
     }
-    downloadVideo(videoId: string): string {
-        return `Downloading https://www.youtube.com/watch?v=${videoId}`;
+    getVideoInfo(
+        videoId: string
+    ): { title: string; length: string } | undefined {
+        this.spy.execute();
+        return this.videos.get(videoId);
     }
 }
