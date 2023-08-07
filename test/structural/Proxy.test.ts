0d1c013be675983f7eaab184d711e9aa014e7db1
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-07 17:05:06 -0300

    refactor[test]: make more explicity and add spies

--- test/structural/Proxy.test.ts
+++ test/structural/Proxy.test.ts
@@ -1,16 +1,51 @@
 import CachedYoutubeService from "../../src/structural/proxy/CachedYoutubeService";
+import Spy from "../../src/structural/proxy/Spy";
 import ThirdPartyYoutubeService from "../../src/structural/proxy/ThirdPartyYoutubeService";
 
 test("Should return a video info if cached or create a new one if not", function () {
-    const youtubeService = new ThirdPartyYoutubeService();
-    const youtubeProxy = new CachedYoutubeService(youtubeService);
-    const videoInfo = youtubeProxy.getVideoInfo("dQw4w9WgXcQ");
-    expect(videoInfo).toEqual({
-        title: "Rick Astley - Never Gonna Give You Up (Official Music Video)",
-        length: "3:32",
-    });
-    const downloadVideo = youtubeProxy.downloadVideo("dQw4w9WgXcQ");
-    expect(downloadVideo).toEqual(
-        "Downloading https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    const thirdPartyYoutubeServiceSpy = new Spy();
+    const cachedYoutubeServiceSpy = new Spy();
+    const youtubeService = new ThirdPartyYoutubeService(
+        thirdPartyYoutubeServiceSpy
     );
+    const youtubeProxy = new CachedYoutubeService(
+        youtubeService,
+        cachedYoutubeServiceSpy
+    );
+    const videoInfo = [
+        youtubeProxy.getVideoInfo("9bZkp7q19f0"),
+        youtubeProxy.getVideoInfo("dQw4w9WgXcQ"),
+        youtubeProxy.getVideoInfo("foE1mO2yM04"),
+        youtubeProxy.getVideoInfo("9bZkp7q19f0"),
+        youtubeProxy.getVideoInfo("dQw4w9WgXcQ"),
+        youtubeProxy.getVideoInfo("foE1mO2yM04"),
+    ];
+    expect(videoInfo).toEqual([
+        {
+            title: "PSY - GANGNAM STYLE(강남스타일) M/V",
+            length: "4:12",
+        },
+        {
+            title: "Rick Astley - Never Gonna Give You Up (Official Music Video)",
+            length: "3:32",
+        },
+        {
+            title: "Mike Posner - I Took A Pill In Ibiza (Seeb Remix) (Explicit)",
+            length: "3:56",
+        },
+        {
+            title: "PSY - GANGNAM STYLE(강남스타일) M/V",
+            length: "4:12",
+        },
+        {
+            title: "Rick Astley - Never Gonna Give You Up (Official Music Video)",
+            length: "3:32",
+        },
+        {
+            title: "Mike Posner - I Took A Pill In Ibiza (Seeb Remix) (Explicit)",
+            length: "3:56",
+        },
+    ]);
+    expect(thirdPartyYoutubeServiceSpy.getRunCount()).toEqual(3);
+    expect(cachedYoutubeServiceSpy.getRunCount()).toEqual(6);
 });
