4849d437ef272fc4833fbe694ffb4cb84c271863
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-29 18:26:22 -0300

    refactor[test]: change CheckIP to IPValidation

--- test/behavioral/ChainOfResponsability.test.ts
+++ test/behavioral/ChainOfResponsability.test.ts
@@ -1,13 +1,13 @@
 import CheckEnd from "../../src/behavioral/chain_of_responsibility/CheckEnd";
-import CheckIP from "../../src/behavioral/chain_of_responsibility/CheckIP";
 import PasswordValidation from "../../src/behavioral/chain_of_responsibility/PasswordValidation";
 import UsernameValidation from "../../src/behavioral/chain_of_responsibility/UsernameValidation";
 import User from "../../src/behavioral/chain_of_responsibility/User";
+import IPValidation from "../../src/behavioral/chain_of_responsibility/IPValidation";
 
 test("Should authenticate and autorize the user without any errors", function () {
     const firstUser = new User("user1", "password1", "172.10.1.1");
     const checkEnd = new CheckEnd();
-    const checkIP = new CheckIP(checkEnd);
+    const checkIP = new IPValidation(checkEnd);
     const checkPassword = new PasswordValidation(checkIP);
     const checkUsername = new UsernameValidation(checkPassword);
     expect(() => checkUsername.execute(firstUser)).not.toThrow(Error);
