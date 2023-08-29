bfa1dcc5f5284356449617377de3b5900b2ba6f2
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-29 18:19:20 -0300

    refactor: change CheckPassword to PasswordValidation

--- src/behavioral/chain_of_responsibility/PasswordValidation.ts
+++ src/behavioral/chain_of_responsibility/PasswordValidation.ts
@@ -1,7 +1,7 @@
 import User from "./User";
 import UserValidation from "./UserValidation";
 
-export default class CheckPassword implements UserValidation {
+export default class PasswordValidation implements UserValidation {
     constructor(readonly next?: UserValidation) {}
     execute(user: User): void {
         if (user.password !== "password1") throw new Error("Wrong password");
