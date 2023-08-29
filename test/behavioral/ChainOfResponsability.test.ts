2b6d91dff37247966ac9c0cde38403edc25b4ddc
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-29 18:28:24 -0300

    refactor[test]: change CheckEnd to NoValidation

--- test/behavioral/ChainOfResponsability.test.ts
+++ test/behavioral/ChainOfResponsability.test.ts
@@ -1,4 +1,4 @@
-import CheckEnd from "../../src/behavioral/chain_of_responsibility/CheckEnd";
+import NoValidation from "../../src/behavioral/chain_of_responsibility/NoValidation";
 import PasswordValidation from "../../src/behavioral/chain_of_responsibility/PasswordValidation";
 import UsernameValidation from "../../src/behavioral/chain_of_responsibility/UsernameValidation";
 import User from "../../src/behavioral/chain_of_responsibility/User";
@@ -6,7 +6,7 @@ import IPValidation from "../../src/behavioral/chain_of_responsibility/IPValidat
 
 test("Should authenticate and autorize the user without any errors", function () {
     const firstUser = new User("user1", "password1", "172.10.1.1");
-    const checkEnd = new CheckEnd();
+    const checkEnd = new NoValidation();
     const checkIP = new IPValidation(checkEnd);
     const checkPassword = new PasswordValidation(checkIP);
     const checkUsername = new UsernameValidation(checkPassword);
@@ -15,7 +15,7 @@ test("Should authenticate and autorize the user without any errors", function ()
 
 test("Should not authenticate and autorize the user", function () {
     const secondUser = new User("user1", "password1", "173.10.1.1");
-    const checkEnd = new CheckEnd();
+    const checkEnd = new NoValidation();
     const checkIP = new CheckIP(checkEnd);
     const checkPassword = new PasswordValidation(checkIP);
     const checkUsername = new UsernameValidation(checkPassword);
