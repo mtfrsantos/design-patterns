44dc24dd44377e76046970c629e01331ad9e89f4
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-29 18:31:19 -0300

    refactor[test]: fix variables names

--- test/behavioral/ChainOfResponsability.test.ts
+++ test/behavioral/ChainOfResponsability.test.ts
@@ -5,19 +5,19 @@ import User from "../../src/behavioral/chain_of_responsibility/User";
 import IPValidation from "../../src/behavioral/chain_of_responsibility/IPValidation";
 
 test("Should authenticate and autorize the user without any errors", function () {
-    const firstUser = new User("user1", "password1", "172.10.1.1");
-    const checkEnd = new NoValidation();
-    const checkIP = new IPValidation(checkEnd);
-    const checkPassword = new PasswordValidation(checkIP);
-    const checkUsername = new UsernameValidation(checkPassword);
-    expect(() => checkUsername.execute(firstUser)).not.toThrow(Error);
+    const user = new User("user1", "password1", "172.10.1.1");
+    const noValidation = new NoValidation();
+    const ipValidation = new IPValidation(noValidation);
+    const passwordValidation = new PasswordValidation(ipValidation);
+    const usernameValidation = new UsernameValidation(passwordValidation);
+    expect(() => usernameValidation.execute(user)).not.toThrow(Error);
 });
 
 test("Should not authenticate and autorize the user", function () {
-    const secondUser = new User("user1", "password1", "173.10.1.1");
-    const checkEnd = new NoValidation();
-    const checkIP = new CheckIP(checkEnd);
-    const checkPassword = new PasswordValidation(checkIP);
-    const checkUsername = new UsernameValidation(checkPassword);
-    expect(() => checkUsername.execute(secondUser)).toThrow(Error);
+    const user = new User("user1", "password1", "173.10.1.1");
+    const noValidation = new NoValidation();
+    const ipValidation = new IPValidation(noValidation);
+    const passwordValidation = new PasswordValidation(ipValidation);
+    const usernameValidation = new UsernameValidation(passwordValidation);
+    expect(() => usernameValidation.execute(user)).toThrow(Error);
 });
