adbd4f102a5608e8f685184fa0c974fa992c24c5
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-29 18:26:10 -0300

    refactor: change CheckIP to IPValidation

--- src/behavioral/chain_of_responsibility/IPValidation.ts
+++ src/behavioral/chain_of_responsibility/IPValidation.ts
@@ -1,7 +1,7 @@
 import User from "./User";
 import UserValidation from "./UserValidation";
 
-export default class CheckIP implements UserValidation {
+export default class IPValidation implements UserValidation {
     constructor(readonly next?: UserValidation) {}
     execute(user: User): void {
         if (user.ip !== "172.10.1.1") throw new Error("Wrong IP");
