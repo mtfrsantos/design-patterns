87fb62e94058a7f59a4de639125a68fafe1d79b1
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-08-29 18:19:29 -0300

    refactor[test]: change CheckPassword to PasswordValidation

--- test/behavioral/ChainOfResponsability.test.ts
+++ test/behavioral/ChainOfResponsability.test.ts
@@ -1,6 +1,6 @@
 import CheckEnd from "../../src/behavioral/chain_of_responsibility/CheckEnd";
 import CheckIP from "../../src/behavioral/chain_of_responsibility/CheckIP";
-import CheckPassword from "../../src/behavioral/chain_of_responsibility/CheckPassword";
+import PasswordValidation from "../../src/behavioral/chain_of_responsibility/PasswordValidation";
 import UsernameValidation from "../../src/behavioral/chain_of_responsibility/UsernameValidation";
 import User from "../../src/behavioral/chain_of_responsibility/User";
 
@@ -8,7 +8,7 @@ test("Should authenticate and autorize the user without any errors", function ()
     const firstUser = new User("user1", "password1", "172.10.1.1");
     const checkEnd = new CheckEnd();
     const checkIP = new CheckIP(checkEnd);
-    const checkPassword = new CheckPassword(checkIP);
+    const checkPassword = new PasswordValidation(checkIP);
     const checkUsername = new UsernameValidation(checkPassword);
     expect(() => checkUsername.execute(firstUser)).not.toThrow(Error);
 });
@@ -17,7 +17,7 @@ test("Should not authenticate and autorize the user", function () {
     const secondUser = new User("user1", "password1", "173.10.1.1");
     const checkEnd = new CheckEnd();
     const checkIP = new CheckIP(checkEnd);
-    const checkPassword = new CheckPassword(checkIP);
+    const checkPassword = new PasswordValidation(checkIP);
     const checkUsername = new UsernameValidation(checkPassword);
     expect(() => checkUsername.execute(secondUser)).toThrow(Error);
 });
