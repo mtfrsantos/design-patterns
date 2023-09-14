7fb1c9f9761f04302f1267542614348bd0f86e5f
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-09-14 15:24:14 -0300

    fix: the collection creates its own iterators

--- test/behavioral/Iterator.test.ts
+++ test/behavioral/Iterator.test.ts
@@ -1,11 +1,9 @@
 import ArrayGenerator from "../../src/behavioral/iterator/ArrayGenerator";
 import StringArray from "../../src/behavioral/iterator/concrete_collection/StringArray";
-import ReversedStringArrayIterator from "../../src/behavioral/iterator/concrete_iterator/ReversedStringArrayIterator";
-import UppercaseStringArrayIterator from "../../src/behavioral/iterator/concrete_iterator/UppercaseStringArrayIterator";
 
 test("Should get an uppercase array of strings", function () {
     const collection = new StringArray(["a", "b", "c", "d"]);
-    const iterator = new UppercaseStringArrayIterator(collection);
+    const iterator = collection.createUppercaseStringArrayIterator();
     const arrayGenerator = new ArrayGenerator(iterator);
     const uppercaseArray = arrayGenerator.execute();
     expect(uppercaseArray).toEqual(["A", "B", "C", "D"]);
@@ -13,7 +11,7 @@ test("Should get an uppercase array of strings", function () {
 
 test("Should get an reversed array of strings", function () {
     const collection = new StringArray(["a", "b", "c", "d"]);
-    const iterator = new ReversedStringArrayIterator(collection);
+    const iterator = collection.createReversedStringArrayIterator();
     const arrayGenerator = new ArrayGenerator(iterator);
     const reversedArray = arrayGenerator.execute();
     expect(reversedArray).toEqual(["d", "c", "b", "a"]);
