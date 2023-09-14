498f95cbe43f82d849ac0c9641c66a4a22587fa6
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-09-14 15:21:17 -0300

    fix: StringArray implements StringArrayCollection

--- src/behavioral/iterator/concrete_collection/StringArray.ts
+++ src/behavioral/iterator/concrete_collection/StringArray.ts
@@ -1,5 +1,16 @@
-export default class StringArray {
+import StringArrayCollection from "../collection/StringArrayCollection";
+import StringIterator from "../iterator/StringIterator";
+import UppercaseStringArrayIterator from "../concrete_iterator/UppercaseStringArrayIterator";
+import ReversedStringArrayIterator from "../concrete_iterator/ReversedStringArrayIterator";
+
+export default class StringArray implements StringArrayCollection {
     constructor(private stringCollection: string[]) {}
+    createUppercaseStringArrayIterator(): StringIterator {
+        return new UppercaseStringArrayIterator(this.stringCollection);
+    }
+    createReversedStringArrayIterator(): StringIterator {
+        return new ReversedStringArrayIterator(this.stringCollection);
+    }
     getStringCollection(): string[] {
         return this.stringCollection;
     }
