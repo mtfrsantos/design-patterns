492c44b73c46edda5cc5536c681e6116b1fc9bfb
Matheus Freitas Santos <mtfrsantos@gmail.com>
2023-09-22 09:39:10 -0300

    refactor: move ConcreteQueryHandler to concrete_mediators directory

--- src/behavioral/mediator/concrete_mediators/ConcreteQueryHandler.ts
+++ src/behavioral/mediator/concrete_mediators/ConcreteQueryHandler.ts
@@ -1,8 +1,8 @@
 // Concrete mediator
-import GetAllUsersQuery from "./GetAllUsersQuery";
-import GetUserByIdQuery from "./GetUserByIdQuery";
-import Query from "./Query";
-import QueryHandler from "./QueryHandler";
+import GetAllUsersQuery from "../concrete_components/GetAllUsersQuery";
+import GetUserByIdQuery from "../concrete_components/GetUserByIdQuery";
+import Query from "../components/Query";
+import QueryHandler from "../mediator/QueryHandler";
 
 export default class ConcreteQueryHandler implements QueryHandler {
     execute(query: Query): string {
