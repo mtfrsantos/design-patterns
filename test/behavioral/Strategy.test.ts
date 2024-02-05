da2cbeb804b3630a59167fbee2a509ab4570f7c3
Matheus Freitas Santos <mtfrsantos@gmail.com>
2024-02-05 14:39:28 -0300

    refactor[test]: update imports

--- test/behavioral/Strategy.test.ts
+++ test/behavioral/Strategy.test.ts
@@ -1,7 +1,7 @@
-import AddStrategy from "../../src/behavioral/strategy/AddStrategy";
-import Context from "../../src/behavioral/strategy/Context";
-import MultiplyStrategy from "../../src/behavioral/strategy/MultiplyStrategy";
-import SubtractStrategy from "../../src/behavioral/strategy/SubtractStrategy";
+import AddStrategy from "../../src/behavioral/strategy/concrete_strategies/AddStrategy";
+import Context from "../../src/behavioral/strategy/context/Context";
+import MultiplyStrategy from "../../src/behavioral/strategy/concrete_strategies/MultiplyStrategy";
+import SubtractStrategy from "../../src/behavioral/strategy/concrete_strategies/SubtractStrategy";
 
 test("Should change behave of the context", function () {
     const context = new Context();
